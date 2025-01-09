ef6ccb0d320f89c437137c45e3bf8b302195270f
Gabriel Lopes <gabriejob1@outlook.com>
2025-01-08 21:36:32 -0300

    new item view model

---
+++ todo-list/NewItemViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  NewItemViewModel.swift
+//  todo-list
+//
+//  Created by Gabriel Lopes on 07/01/25.
+//
+
+import Foundation
+
+
+class NewItemViewModel: ObservableObject {
+    @Published var title = ""
+    @Published var dateDue =  Date()
+    @Published var showAlert = false
+    
+    init() {}
+    
+    func save() {}
+    
+    var canSave: Bool {
+        guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        guard dateDue >= Date().addingTimeInterval(-86400) else {
+            return false
+        }
+        
+        return true
+    }
+}
